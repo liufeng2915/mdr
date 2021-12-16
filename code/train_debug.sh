d9460c4c2b7633ff2b94c400bd21a92179d5feec
Feng Liu <46528798+liuf1990@users.noreply.github.com>
2021-12-16 00:12:00 -0500

    Delete train_debug.sh

--- code/train_debug.sh
+++
@@ -1,4 +0,0 @@
-
-CUDA_VISIBLE_DEVICES=0 python -m pdb training/exp_runner.py --conf ./confs/ShapeNet-triplets.conf --batch_size 4 --is_continue
-
-#CUDA_VISIBLE_DEVICES=0 python training/exp_runner.py --conf ./confs/ShapeNet-triplets.conf --batch_size 1
